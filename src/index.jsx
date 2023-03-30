89baeb39e2b2575a3331196ab44513d67316ce98
hnejman <108231655+hnejman@users.noreply.github.com>
2023-03-30 13:12:34 +0200

    ...

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,8 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>
+  <BrowserRouter basename="/ goit-react-hw-01-components/">
+    <App />
+  </BrowserRouter>
 );
 
